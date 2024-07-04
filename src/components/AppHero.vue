716d5542943d2bfeec88ad5c533cba6928622c35
Samgrass <samuelegrassetti01@gmail.com>
2024-07-04 18:05:41 +0200

    BONUS: aggiungo jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<script>
+    export default {
+        name: "AppHero"
+    }
+</script>
+
+<template>
+    <div class="jumbotron">
+        <h2>Questo è un jumbotron</h2>
+    </div>
+</template>
+
+<style>
+    .jumbotron {
+        background-image: url(../assets/jumbotron.png);
+        height: 400px;
+        background-size: cover;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    h2 {
+        font-size: 3rem;
+        color: rgb(200, 212, 20);
+    }
+</style>

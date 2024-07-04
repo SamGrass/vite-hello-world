018567db3510ad69c7edd2a1b84fee48530c535c
Samgrass <samuelegrassetti01@gmail.com>
2024-07-04 17:44:53 +0200

    Creazione component AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        name: "AppTitle",
+    }
+</script>
+
+<template>
+    <h1>
+        La mia prima app con Vite! 
+    </h1>
+</template>
+
+<style scoped>
+h1 {
+    font-size: 4rem;
+    color: red;
+}
+</style>
